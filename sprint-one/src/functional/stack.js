f91f02a644e735b63c53459f2f5395b1c4db0c01
Adam Whitman <adam.whitmanpt@gmail.com>
2020-10-07 16:16:54 -0700

    Need to complete last part of first section, possibly recursion?

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -4,15 +4,39 @@ var Stack = function() {
   // Use an object with numeric keys to store values
   var storage = {};
 
+  var numKey = 0;
+
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[numKey] = value;
+    numKey++;
   };
 
-  someInstance.pop = function() {
+  someInstance.pop = function(value) {
+    storage[numKey] = value;
+    numKey--;
   };
 
-  someInstance.size = function() {
+  someInstance.size = function(value) {
+    if (numKey < 0) {
+      return 0;
+    } else {
+      return numKey;
+    }
   };
 
   return someInstance;
 };
+
+/*
+[1, 2, 3, 4]
+var size = 4;
+var storage = {
+  0 : 1,
+  1 : 2,
+  2 : 3,
+  size : 4
+};
+
+storage[size] = 4
+*/
