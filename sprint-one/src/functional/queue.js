e5a17a0fd40c5b92fbd473c2c045ee8300c568d3
Adam Whitman <adam.whitmanpt@gmail.com>
2020-10-08 17:39:25 -0700

    Completed functional classes

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,33 +1,45 @@
-var Queue = function() {
+var Queue = function () {
   var someInstance = {};
 
   // Use an object with numeric keys to store values
   var storage = {};
 
-  var numKey = 0;
+  var frontOfLine = 0;
+
+  var backOfLine = 0;
+
 
   // Implement the methods below
 
-  someInstance.enqueue = function(value) {
-    storage[numKey] = value;
-    numKey++;
+  someInstance.enqueue = function (value) {
+    storage[backOfLine] = value;
+    backOfLine++;
   };
 
-  someInstance.dequeue = function() {
-    for (var keys in storage) {
-      var result = storage[keys];
-      storage[keys] = undefined;
-      numKey--;
-      return result;
-    }
+  someInstance.dequeue = function () {
+    //I: Obj
+    //O: Modified obj
+    //SE: None
+    //EC: None I can think of
+
+    // let order equal array <-- poor time complexity, refactor required
+    // push enque result into array
+    // dequeu check first index in array
+    // compare equality to storage key/value pair
+    // set storage key from equality check to undefined
+    // return storage object
+
+    var result = storage[frontOfLine];
+
+    delete storage[frontOfLine];
+
+    frontOfLine++;
+
+    return result;
   };
 
-  someInstance.size = function() {
-    if (numKey < 0) {
-      return 0;
-    } else {
-      return numKey;
-    }
+  someInstance.size = function () {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
