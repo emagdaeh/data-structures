60e4d21af52a25d5b4ea53dcf45e4e16287a15ae
Adam Whitman <adam.whitmanpt@gmail.com>
2020-10-10 15:48:29 -0700

    Semi-built contains and newTree, working on addChildren

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,21 +3,53 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = []; // fix me
+  //Value of children should be an array
+  //Place to put new nodes from addChild method
+  //Incorporate value for further nodes to target
+  _.extend(newTree, treeMethods);
+  console.log('new tree: ', newTree);
 
   return newTree;
 };
 
 var treeMethods = {};
 
-treeMethods.addChild = function(value) {
+treeMethods.addChild = function(thing) {
+  if (this.newTree === undefined) {
+    this.newTree[this.value] = thing;
+    console.log(this.newTree);
+  }
+  //I: Some value
+  //O: Node
+  // Set variable to hold value of child
+  // Set variable to be a target of a node Tree
+  // Add to the Tree
+  if (this.newTree !== undefined) {
+    console.log('this.children: ', this.children);
+    this.children.push(thing);
+  }
 };
 
 treeMethods.contains = function(target) {
+  //I: value
+  //O: boolean
+  //If top of tree equals target
+  //return true
+  //else recurse over child nodes
+  //return false at end of function
+  if (target === this.value) {
+    return true;
+  } else {
+    for (var i = 0; i < this.children.length; i++) {
+      this.contains(this.children[i]);
+    }
+  }
+  return false;
 };
 
 
 
 /*
- * Complexity: What is the time complexity of the above functions?
+ * Complexity: What is the time complexity of the above functions? Linear
  */
