c33b6cce4c6ae936070fd700ef993484e9fe2f98
Adam Whitman <adam.whitmanpt@gmail.com>
2020-10-11 20:50:11 -0700

    Working on pseudocode for binary search

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,42 @@
+/*
 var BinarySearchTree = function(value) {
 };
-
+*/
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+
+var BinarySearchTree = function(value) {
+  //I: Target value to search for in the tree
+  //O: Value
+  //C: Value < 0
+  //E: Empty data set
+  //Left iteration focused on decrementing the search
+  //Right iteration focused on incrementing
+  //Cap children at 2 (left and right)
+  //Contains function to compare input vs value at current iteration
+  //Conditional to dictate going into L or R tree
+  //Insert function place new value as child of closest node value
+
+  var obj = Object.create(BinarySearchTree.methods);
+
+
+
+  return obj;
+};
+
+BinarySearchTree.methods = {};
+
+BinarySearchTree.methods.insert = function() {
+
+};
+
+BinarySearchTree.methods.contains = function() {
+
+};
+
+BinarySearchTree.methods.depthFirstLog = function() {
+
+};
