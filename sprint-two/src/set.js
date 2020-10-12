f6acc425731e27ae459803626c38f5e89964b492
Adam Whitman <adam.whitmanpt@gmail.com>
2020-10-11 21:31:21 -0700

    Finished pseudocode for set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -7,12 +7,21 @@ var Set = function() {
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  //Iterate over set storage
+  //Conditional to check item for equivalence
+  //If not, add item to set
 };
 
 setPrototype.contains = function(item) {
+  //Iterate over set storage
+  //If item exists in storage return true
+  //Else return false
 };
 
 setPrototype.remove = function(item) {
+  //Iterate over set storage
+  //Conditional check item for equivalence
+  //If present, delete item from set
 };
 
 /*
